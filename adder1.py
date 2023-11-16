41c1b30d2e2319e4685ad70f58de33c29b67c130
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-16 17:45:46 +0530

    Create adder1.py

---
+++ adder1.py
@@ -0,0 +1,14 @@
+# Define the adder function to add two numbers
+def adder(a, b):
+    return a + b
+
+
+# Call the function to add two strings, two lists, and two floating points
+result_string = adder("Hello ", "World!")
+result_list = adder([1, 2, 3], [4, 5, 6])
+result_float = adder(35.97, 10.987)
+
+# Print the results
+print("String result:", result_string)
+print("List result:", result_list)
+print("Float result:", result_float)
